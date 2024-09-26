652149d2db525e67b4d6ec9a8fdf7a34f5d114c8
John Bostater <jdbost8r@gmail.com>
2024-09-25 19:29:15 -0700

    Add files via upload

---
+++ CardGame.py
@@ -0,0 +1,10 @@
+"""
+[Author]: John Bostater
+
+[Start Date]: 9/25/24
+
+
+[Description]:
+    This Python script contains code for Sockets to communicate with eachother via different hosts
+    
+"""
