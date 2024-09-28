5c5d0860ac9a4060a1a733711e8fdb5a2c5126aa
John Bostater <jdbost8r@gmail.com>
2024-09-27 22:12:19 -0700

    Add files via upload

--- Tracker.py
+++ Tracker.py
@@ -6,36 +6,30 @@
 [Description]:
     {Networking Project}
 
-    This Python script contains code that will track the game as well as server-client processes
+    Server-Python Script, maintains state information of players and ongoing games.
+    This file is able to respond to players via a text-based user interface.
+
+    When executing the Tracker Script a paramater for the Port Number must be defined
+      [Command]:
+        > python3 Tracker.py 123456     //123456 is the port number in this case, add ip paramater?
+
+
+    This Server contains a fixed IP address and Port Number that the Player's/Player objects
+    will use for sending and receiving information about the game
 """
 
 
 #Relevant Libaries
 #-----------------
 import socket
-import random
+from Player import*
 #-----------------
 
 
 
 #Global Variables
-#--------------------------------------------------------------------------------
-
-#This is the main Card deck that the Dealer will deal use
-cardDeck = [
-    #Clubs
-    "AC", "2C", "3C", "4C", "5C", "6C", "7C", "8C", "9C", "10C", "JC", "QC", "KC",
-
-    #Spades
-    "AS", "2S", "3S", "4S", "5S", "6S", "7S", "8S", "9S", "10S", "JS", "QS", "KS",
-
-    #Hearts
-    "AH", "2H", "3H", "4H", "5H", "6H", "7H", "8H", "9H", "10H", "JH", "QH", "KH",
-
-    #Diamonds
-    "AD", "2D", "3D", "4D", "5D", "6D", "7D", "8D", "9D", "10D", "JD", "QD", "KD"
-]
-#--------------------------------------------------------------------------------
+#----------------
+#----------------
 
 
 
@@ -51,47 +45,31 @@ cardDeck = [
 
 #Add Player
 def addPlayer(newPlayer):
-    #This function will add all of the players to an array of CardPlayer Objects
+    #This function will add a new player to the game
 
     return '0'
 
+
+#Remove Player
 def removePlayer():
-    #This function
+    #This function will remove the player from the game
 
-    return '0'
-
-
-#Deal Cards
-def dealCards(playerArray):
-
-    #Deal each player 6 cards
-    for players in playerArray:
-        #Add's 6 cards to each players deck
-        
-        #Randomly generate a number that will correspond to a card in the deck       
-        shuffleNum = random.randint(0, 51)
-
-        #Based on the random number pull the corresponding card out
-        match shuffleNum:
-            case 0: 
-                print('okay')
-                return "hello"
-
-
- 
-#Determine if card_1 beats card_2
-def compareCards(card_1, card_2):
-    #Add code here...
-    
-    return 0
+    return '0' 
 #----------------------------
 
 
 
-#Driver Space
+#Main/Driver Space
 #----------------------------
 
-#DEBUG
-for player in cardDeck:
-    print(player + " ")
+#Text Menu for the Player to Start A new Game, Join and Exisitng one, or delete it??
+#[Requirements]:
+    #Make a Game
+
+    #Join A game
+
+
+
+#Make a player object
+player0 = CardPlayer()
 #----------------------------
