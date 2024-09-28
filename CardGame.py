cca64d48307d30852144fdf0a4bf5334d0181437
John Bostater <jdbost8r@gmail.com>
2024-09-27 18:38:41 -0700

    Add files via upload

--- CardGame.py
+++ CardGame.py
@@ -3,8 +3,41 @@
 
 [Start Date]: 9/25/24
 
-
 [Description]:
-    This Python script contains code for Sockets to communicate with eachother via different hosts
-    
+    Networking Project...
+
+    This Python script contains code for Sockets to communicate with eachother through different hosts.
+    These hosts can then play the card game 'Golf' as followed in the tutorial. 
 """
+
+#Relevant Libaries
+#-----------------
+import socket
+import random
+#-----------------
+
+
+#Server and Client Code Space
+#----------------------------
+
+
+#----------------------------
+
+
+#Player Class Object
+class CardPlayer:
+  #Array that will hold the players uniquely dealed cards
+    playerDeck = []
+
+  #
+
+  #Other variables here...
+
+
+#User Functions
+
+
+#Server Functions
+
+
+#Driver Function
